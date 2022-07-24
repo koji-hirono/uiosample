e59fa2e66576364f930b4a56ff52b9b240105865
Koji Hirono <koji.hirono@gmail.com>
2022-07-24 16:55:33 +0000

    tunnel: Use worker

--- cmd/tunnel/server.go
+++ cmd/tunnel/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"os"
+	"sync"
+
+	"uiosample/worker"
 )
 
 type Server struct {
@@ -17,13 +21,10 @@ func NewServer(port1, port2 *Port) *Server {
 }
 
 func (s *Server) Serve(sig chan os.Signal) {
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
 	pkts := make([][]byte, 32, 32)
-	for {
-		select {
-		case <-sig:
-			return
-		default:
-		}
+	worker.Worker(ctx, &wg, 2, func() {
 		// N3
 		n := s.port1.RxBurst(pkts)
 		for i := 0; i < n; i++ {
@@ -34,5 +35,9 @@ func (s *Server) Serve(sig chan os.Signal) {
 		for i := 0; i < n; i++ {
 			s.procN6(pkts[i])
 		}
-	}
+	})
+	defer wg.Wait()
+
+	<-sig
+	cancel()
 }
