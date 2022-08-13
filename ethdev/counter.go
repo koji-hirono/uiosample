db1870f93b414dea80faea195005f3ee26790ba4
Koji Hirono <koji.hirono@gmail.com>
2022-08-13 07:07:09 +0000

    ethdev: Add SumCounter

--- ethdev/counter.go
+++ ethdev/counter.go
@@ -5,6 +5,29 @@ type Counter interface {
 	Clear()
 }
 
+type SumCounter struct {
+	counters []Counter
+	val      uint64
+}
+
+func NewSumCounter(counters ...Counter) *SumCounter {
+	return &SumCounter{counters: counters}
+}
+
+func (c *SumCounter) Value() uint64 {
+	for _, cc := range c.counters {
+		c.val += cc.Value()
+	}
+	return c.val
+}
+
+func (c *SumCounter) Clear() {
+	for _, cc := range c.counters {
+		cc.Clear()
+	}
+	c.val = 0
+}
+
 type CounterGroup struct {
 	RxPackets Counter
 	TxPackets Counter
